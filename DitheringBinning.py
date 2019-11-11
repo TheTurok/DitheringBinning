6bef9acd01c9e60ccc8a6c1ec28eb36a86d018d1
Timothy Choi <TurokChoi@gmail.com>
2019-11-10 20:43:00 -0800

    first commit

---
+++ DitheringBinning.py
@@ -0,0 +1,7 @@
+import os
+import time
+
+if __name__ == "__main__":
+    start_time = time.time()
+    end_time = time.time()
+    print("Total time to run this program: " + str(end_time-start_time) + " seconds")
---
+++ TestDitheringBinning.py
@@ -0,0 +1,12 @@
+import unittest
+import DitheringBinning
+
+class TestDitheringBinning(unittest.TestCase):
+    """Testing Dithering functions"""
+
+    def test_this(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
